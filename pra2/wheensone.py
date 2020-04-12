16958432475b94785b6d653f394f61b1d525b5b0
hlouzada <hendriklouzada@gmail.com>
2020-04-12 18:13:06 -0300

    ADD: R4 value for Vg=0 in fitted function

--- pra2/wheensone.py
+++ pra2/wheensone.py
@@ -15,7 +15,23 @@ params = pmodel.make_params(R1=150,R2=323,R3=222,V=10)
 
 result = pmodel.fit(v, params, R4=r4)
 
-print(result.fit_report())
+res = result.fit_report()
+
+print(res)
+
+coefs = res[res.find("[[Variables]]")+13:res.find("[[Correlations]]")].split("\n")[1:5]
+
+coefs = [coef.split() for coef in coefs]
+
+Va = float(coefs[3][1])
+R3a = float(coefs[2][1])
+R2a = float(coefs[1][1])
+R1a = float(coefs[0][1])
+Vga = 0
+
+R4V0 = R3a*((1/(R2a/(R1a+R2a)-Vga/Va)) - 1)
+
+print("R_4 for Vg = 0: {0}".format(R4V0))
 
 #print("a = {0} +- {2} b = {1} +- {3}" .format(*params,*np.sqrt(np.diag(pcov))))
 
@@ -25,8 +41,8 @@ plt.rcParams.update({'font.size': 10})
 
 plt.scatter(r4, v, label='Dados', color='#212121', s=25,zorder=5)
 
-plt.xlabel(r'R_4 $(\Omega)$',fontsize=10)
-plt.ylabel(r'V_g $(V)$',fontsize=10)
+plt.xlabel(r'$R_4\,(\Omega)$',fontsize=10)
+plt.ylabel(r'$V_g\,(V)$',fontsize=10)
 plt.title(r'Ponte de Wheastone',fontsize=15)
 #plt.figtext(.5,.930,'Resistor 1', fontsize=18, ha='center')
 
