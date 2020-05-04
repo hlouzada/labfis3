cf3a605c659c31f3eb710ca20a3c48b9015067e4
hlouzada <hendriklouzada@gmail.com>
2020-05-04 09:20:47 -0300

    cabo parte 1, mas ainda falta melhorar o grafico log

--- pra3/parte1_2.py
+++ pra3/parte1_2.py
@@ -3,7 +3,7 @@ import numpy as np
 from labfis import labfloat
 import lmfit
 
-dados = np.loadtxt('carga')
+dados = np.loadtxt('carga2')
 
 vc = np.array([k[0] for k in dados])
 t = np.array([k[2] for k in dados])
@@ -11,11 +11,11 @@ t = np.array([k[2] for k in dados])
 print(vc)
 print(t)
 
-vo = 2.2
+vo = 10
 
-capacitor = lambda t,Vo,Tal: Vo*(1-np.exp(-t/Tal))
+capacitor = lambda t,Vo,Tal: Vo*np.exp(-t/Tal)
 cmodel = lmfit.Model(capacitor)
-params = cmodel.make_params(Vo=vo,Tal=106e-6*97e3)
+params = cmodel.make_params(Vo=vo,Tal=106e-6*224e3)
 result = cmodel.fit(vc, params, t=t)
 res = result.fit_report()
 
@@ -24,31 +24,33 @@ print(res)
 coefs = res[res.find("[[Variables]]")+13:res.find("[[Correlations]]")].split("\n")[1:3]
 coefs = [coef.split() for coef in coefs]
 
-Tal = labfloat(coefs[1][1],coefs[1][3])
+print(coefs)
 
-R = labfloat(97e3,1e3)
+Tal = labfloat(coefs[1][1],0)
+
+R = labfloat(224e3,1e3)
 
 C = Tal / R
 
 print("C = {0}".format(C))
 
 xs = np.linspace(1.5,34,200)
-ys = np.array([capacitor(xs[j],float(coefs[0][1]),float(coefs[1][1])) for j in range(len(xs))])
 
+ys = np.array([capacitor(xs[j],float(coefs[0][1]),float(coefs[1][1])) for j in range(len(xs))])
 
 plt.figure()
 
 plt.rcParams.update({'font.size': 10})
 
-plt.plot(xs, ys,'-',label=r'$V_o(1-e^{\frac{-t}{\tau}})$',linewidth=1.8,color='#adadad',zorder=0)
+plt.plot(xs, ys,'-',label=r'$V_o e^{\frac{-t}{\tau}}$',linewidth=1.8,color='#adadad',zorder=0)
 plt.scatter(t, vc, label='Dados', color='#212121', s=25,zorder=5)
 
 plt.xlabel(r'$t\,(s)$',fontsize=10)
-plt.ylabel(r'$V_c\,(V)$',fontsize=10)
+plt.ylabel(r'$V\,(V)$',fontsize=10)
 plt.title(r'Comportamento do Capacitor',fontsize=15)
 plt.legend()
 
-plt.savefig('capacitor.png')
+plt.savefig('capacitor2.png')
 plt.show()
 
 
@@ -58,16 +60,16 @@ plt.rcParams.update({'font.size': 10})
 
 plt.yscale('log')
 
-plt.plot(xs, vo - ys, '-',label=r'$V_o(1-e^{\frac{-t}{RC}})$',linewidth=1.8,color='#adadad',zorder=0)
-plt.plot(t, vo - vc, 'o', label='Dados', color='#212121', markersize=5, zorder=5)
+plt.plot(xs, ys, '-',label=r'$V_o e^{\frac{-t}{RC}}$',linewidth=1.8,color='#adadad',zorder=0)
+plt.plot(t, vc, 'o', label='Dados', color='#212121', markersize=5, zorder=5)
 
 eval("plt.figtext(.312,.145,r'$C = "+C.tex(2,0)+"\,F$',fontsize=10,ha='center')")
 
 plt.xlabel(r'$t\,(s)$',fontsize=10)
-plt.ylabel(r'$log(V_o - V_c)\,(V)$',fontsize=10)
+plt.ylabel(r'$log(V)\,(V)$',fontsize=10)
 plt.title(r'Descarga do Capacitor',fontsize=15)
 plt.legend()
 
-plt.savefig('capacitorlog.png')
+plt.savefig('capacitorlog2.png')
 
 plt.show()
